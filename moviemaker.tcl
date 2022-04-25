599189c78d9f721589951494a2615d26a8aef9ee
MIDST lab <89193539+midstlab@users.noreply.github.com>
2022-04-25 17:48:03 +0300

    Update moviemaker.tcl

--- moviemaker.tcl
+++ moviemaker.tcl
@@ -3,6 +3,6 @@
 set nf [molinfo top get numframes]
 for { set i 0} { $i <= $nf-1 } { incr i } {
 animate goto $i
-render Tachyon dhfWT1.$i.dat "/Applications/VMD/VMD1.9.4a51.app/Contents/vmd/tachyon_MACOSXX86_64" dhfWT1.$i.dat -format PPM -o dhfWT1.$i.ppm
+render Tachyon dhf.$i.dat "/Applications/VMD/VMD1.9.4a51.app/Contents/vmd/tachyon_MACOSXX86_64" dhf.$i.dat -format PPM -o dhf.$i.ppm
 
 }
